6db737576e455c66b659c615b4184e8ebc135a8c
jaskiewm <m.jasski@hotmail.com>
2025-04-05 21:55:11 -0400

    Cypher Book Update
    - Added Dragging of the book
    - Added title bar
    - Removed text editor & document close button
    - Changed colours of buttons

--- scripts/cypher_book.gd
+++ scripts/cypher_book.gd
@@ -3,6 +3,12 @@ extends Control
 var current_page = 0
 var pages = []
 
+# Moving the book
+var mouse_in_title := false
+var drag_offset := Vector2.ZERO
+var dragging := false
+var newPosition := Vector2()
+
 func _ready():
 	visible = false
 	pages = [
@@ -28,12 +34,36 @@ func _on_next_page_pressed() -> void:
 		current_page+=1
 		_show_page(current_page)
 
-
 func _on_back_page_pressed() -> void:
 	if current_page > 0:
 		current_page-=1
 		_show_page(current_page)
 
-
 func _on_button_pressed() -> void:
 	visible = false
+
+# Moving the book
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() && mouse_in_title:
+			dragging = true
+			
+			# Calculate offset between mouse and panel top-left corner
+			drag_offset = get_global_mouse_position() - global_position
+		else:
+			dragging = false
+			
+	elif event is InputEventMouseMotion:
+		if dragging:
+			newPosition = get_viewport().get_mouse_position() - drag_offset
+
+func _physics_process(delta):
+	if dragging:
+		var target_position = get_global_mouse_position() - drag_offset
+		global_position = global_position.lerp(target_position, 0.8)
+
+func _on_title_bar_mouse_entered() -> void:
+	mouse_in_title = true
+
+func _on_title_bar_mouse_exited() -> void:
+	mouse_in_title = false
