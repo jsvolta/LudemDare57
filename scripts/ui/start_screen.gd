3625816c225de4a6cbacc688b4bc78a0c9824042
Dylan Pithia <128931905+dpithia@users.noreply.github.com>
2025-04-05 20:55:24 -0400

    feat: added scrolling intro screen

--- scripts/ui/start_screen.gd
+++ scripts/ui/start_screen.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	anchors_preset = Control.PRESET_FULL_RECT
 	
 func _on_start_button_pressed() -> void:
-	# switches to main screen
+	# switches to main game scene
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn") 
