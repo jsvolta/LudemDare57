9e643d28332f363474bb0a8c67b6372ebbf70ed4
jaskiewm <m.jasski@hotmail.com>
2025-04-06 02:23:35 -0400

    Added Morse Code to State Script
    Added Morse Code to State Script

--- scripts/state.gd
+++ scripts/state.gd
@@ -7,13 +7,20 @@ const background_2 = preload("res://scenes/backgrounds/level_2.tscn")
 # 0 -> key
 # 1 -> end message
 # 2 -> level index
+# 3 -> morse code
 var level_data = [
-["Bubble Wrap SOS","Bubble wrap SOS! We require immediate popping assistance. Your mission is loud and clear.",1],
-["More Cats Please","Greetings, Earthling. Your planet hums with wonder. Especially the part about cats. More, please.",1],
-["Emoji Language","Languages are only spoken with thoughts, emojis may be more efficient!",2],
-["Colourful pictures","Your drawings were analyzed. Our scientists are baffled. Please continue. You may be the key.",3],
-["A missing voice",	"You were never alone. You just forgot how to listen. The universe missed your voice.",4],
-["Questions and voices","You are the conversation. You are the question and the key. We hear you. And now… you hear us too.",5]
+["Bubble Wrap SOS","Bubble wrap SOS! We require immediate popping assistance. Your mission is loud and clear.",1,
+"_ . . . . . _ . _ . . _ . . . _ . . . . . _ . . . . _ . _ . . . _ . _ _ . . _ _ _ . . ."],
+["More Cats Please","Greetings, Earthling. Your planet hums with wonder. Especially the part about cats. More, please.",1,
+"_ . _ . . _ . . . _ . _ . _ . _ . . _ . . . _ . . _ _ . . _ . _ . . . _ . . . . _ . . . . "],
+["Emoji Language","Languages are only spoken with thoughts, emojis may be more efficient!",2,
+". _ . _ _ . . _ _ _ . . . _ . _ . . _ . . _ . . . _ . . _ . _ . _ . _ . . . _ . _ . _ ."],
+["Colourful pictures","Your drawings were analyzed. Our scientists are baffled. Please continue. You may be the key.",3,
+"_ . _ . _ . . . _ _ . . _ . . . . . _ . . . _ . _ . . _ . _ . _ . _ . . _ . _ . . . _ . . _ . ."],
+["A missing voice",	"You were never alone. You just forgot how to listen. The universe missed your voice.",4,
+". _ _ _ . . . _ . . . _ _ . . . _ . _ . _ . . ."],
+["Questions and voices","You are the conversation. You are the question and the key. We hear you. And now… you hear us too.",5,
+"_ . _ . _ . . _ . . _ _ . . . _ . _ _ . . _ . _ . . _ . _ . . . _ . . . _ . . ."]
 ]
 var current_level_data: Array
 
