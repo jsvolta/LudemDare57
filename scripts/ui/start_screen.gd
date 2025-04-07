99aca5ca7316c3e3540e076f478de4eb6ef98a34
jaskiewm <m.jasski@hotmail.com>
2025-04-07 03:47:05 -0400

    Added Quit Button to Start Screen
    Added Quit Button to Start Screen and some positioning changes to allow for both buttons

--- scripts/ui/start_screen.gd
+++ scripts/ui/start_screen.gd
@@ -11,3 +11,8 @@ func _on_start_button_pressed() -> void:
 	TransitionScreen.transition()
 	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
