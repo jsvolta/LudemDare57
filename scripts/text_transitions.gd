564036d11771fb91bb7a298f6d0ec3db4e63171f
Jaden Christie <158334020+jsvolta@users.noreply.github.com>
2025-04-07 10:22:46 -0400

    Fix: object freed bug in text transitions

--- scripts/text_transitions.gd
+++ scripts/text_transitions.gd
@@ -8,7 +8,7 @@ func typewriter(text_box: Control, text: String, speed: float = 40):
 	text_box.text = text
 	text_box.visible_characters = 0
 	_typing_time = 0
-	while text_box.visible_characters < text.length():
+	while (is_instance_valid(text_box) and text_box.visible_characters < text.length()):
 		_typing_time += get_process_delta_time()
 		text_box.visible_characters = speed * _typing_time as int
 		await get_tree().process_frame
