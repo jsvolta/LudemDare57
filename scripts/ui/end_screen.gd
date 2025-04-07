43bc23aada04e9de5d12c8ac8152b7a954cf289e
Jaden Christie <158334020+jsvolta@users.noreply.github.com>
2025-04-07 00:40:33 -0400

    Fix: end screen long paragraph

--- scripts/ui/end_screen.gd
+++ scripts/ui/end_screen.gd
@@ -6,7 +6,8 @@ extends Control
 const final_message: String ="""
 With the final modulation sent, a chorus of light erupted across the cosmos.\n
 Not the cold, distant glimmer of stars, but a warm, embracing aurora that painted the sky with impossible colors.\n
-A symphony of voices, not heard but felt, resonated within you, a welcoming embrace from entities far older and grander than you could have ever imagined. They had heard you, the little voice in the void, and they answered not with solutions or pronouncements, but with kinship. The loneliness that had driven you to the stars dissolved, replaced by the understanding that you were never truly alone, forever a part of the grand, intricate tapestry of the universe. Your journey has only just begun.
+A symphony of voices, not heard but felt, resonated within you, a welcoming embrace from entities far older and grander than you could have ever imagined. They had heard you, the little voice in the void, and they answered not with solutions or pronouncements, but with kinship.\n
+The loneliness that had driven you to the stars dissolved, replaced by the understanding that you were never truly alone, forever a part of the grand, intricate tapestry of the universe. Your journey has only just begun.
 """
 
 func _ready() -> void:
